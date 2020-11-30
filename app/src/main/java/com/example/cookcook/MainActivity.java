e29857e2d0bb6c9c29ff08b5f399bb20dacc24f4
yohan <yohansx11@gmail.com>
2020-11-30 19:04:28 +0700

    pre-release-development

--- app/src/main/java/com/example/cookcook/MainActivity.java
+++ app/src/main/java/com/example/cookcook/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.cookcook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,15 +13,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView tvError;
     EditText etUsername, etPassword;
+    Button btnLogin,btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tvError = findViewById(R.id.tvErrorMsg);
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
         tvError.setVisibility(View.INVISIBLE);
         etUsername.getBackground().setAlpha(50);
         etPassword.getBackground().setAlpha(50);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    void validation(){
+
+    }
+
 }
